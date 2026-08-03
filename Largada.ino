540f04bfee990728661ee92ff233fe82d5e883fc
Guilherme Zanardo <guizanardo2010@gmail.com>
2026-08-03 15:39:07 -0300

    codigo
    Codigo para o arduino

---
+++ Largada.ino
@@ -0,0 +1,48 @@
+int largada;
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+int led5 = 12;
+  
+  void setup() {
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+    pinMode(led4, OUTPUT);
+    pinMode(led5, OUTPUT);
+  }
+
+
+
+void loop () {
+
+
+  digitalWrite(led1,HIGH);
+  delay(1000);
+    
+  digitalWrite(led2, HIGH);
+  delay(1000);
+    
+  digitalWrite(led3, HIGH);
+  delay(1000);
+    
+  digitalWrite(led4, HIGH);
+  delay(1000);
+  
+  digitalWrite(led5,HIGH);
+  delay(1000);
+  
+    
+     digitalWrite(led1,LOW);
+     digitalWrite(led2,LOW);
+     digitalWrite(led3,LOW);
+     digitalWrite(led4,LOW);
+     digitalWrite(led5,LOW);
+  
+  largada = random(500, 5000);
+  delay(largada);
+  
+  delay(4000);
+
+}
